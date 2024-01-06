fbcb68ec93c3883300a9775ec74ba3dc4730f8db
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-01-05 20:14:08 -0600

    created populate function where it displays what values or operators the users clicked

--- script.js
+++ script.js
@@ -34,9 +34,34 @@ function operate(num1,num2,operator){
   }
 }
 
+let displayValue; //idk this is used for a certain step
+const btnTest = document.querySelector(".container");//checks if button is clicked
+const display = document.querySelector("#display")
+
+function populate(btnTest,display){ //figure out how to detect "which button is clicked" then figure out how to populate the screen
+  btnTest.addEventListener('click',function(e){
+    displayValue = e.target.id; //e accesses the object itself
+    console.log(displayValue);
+
+    /*
+    const number = document.createElement('div');
+    number.textContent = displayValue;
+    display.appendChild(number);
+    */
+
+   if(displayValue == "clear"){
+    display.textContent = "0";
+   }else{
+    display.textContent += displayValue; //when doing css create this into its own element 
+   }
+  });
+}
+
 num1 = 2;
 num2 = 5;
 operator = '/';
 
 const test = operate(num1,num2,operator);
 console.log(test);
+
+populate(btnTest,display);
