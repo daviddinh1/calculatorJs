4e8fea013079e6cf1de78bc5b180258601190df1
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-01-07 22:46:40 -0600

    created a feature where calculator can calculate 2 numbers and an operator now

--- script.js
+++ script.js
@@ -39,20 +39,75 @@ const btnTest = document.querySelector(".container");//checks if button is click
 const display = document.querySelector("#display")
 
 function populate(btnTest,display){ //figure out how to detect "which button is clicked" then figure out how to populate the screen
+  let numbers = [];
+  let arr = []; //holds single number if user wants to join them 
+  let number; //holds this "number"
+
   btnTest.addEventListener('click',function(e){
     displayValue = e.target.id; //e accesses the object itself
-    console.log(displayValue);
+    //console.log(displayValue);
 
     /*
     const number = document.createElement('div');
     number.textContent = displayValue;
     display.appendChild(number);
     */
-
+  
    if(displayValue == "clear"){
+    numbers = [];//clears array
     display.textContent = "0";
-   }else{
-    display.textContent += displayValue; //when doing css create this into its own element 
+   }
+   else if(displayValue == "="){
+    number = arr.join(''); //holds third value LFGGG
+    numbers.push(number);
+    numbers.push(displayValue);
+    
+    arr = [];
+    number ="";
+    display.textContent = operate(Number(numbers[0]),Number(numbers[2]),numbers[1]);
+    console.log(operate(Number(numbers[0]),Number(numbers[2]),numbers[1]));
+   }
+   else{
+    display.textContent += displayValue; //when doing css create this into its own element
+    if(displayValue == "+"){ //learn how to do more than 2 numbers learn how to save two numbers then do operation
+      number = arr.join('');
+      numbers.push(number);
+      numbers.push(displayValue);
+      
+      arr = [];
+      number = "";
+    } 
+     else if(displayValue == "-"){ 
+      number = arr.join('');
+      numbers.push(number);
+      numbers.push(displayValue);
+      
+      arr = [];
+      number = "";
+    }
+     else if(displayValue == "*"){ 
+      number = arr.join('');
+      numbers.push(number);
+      numbers.push(displayValue);
+      
+      arr = [];
+      number = "";
+      console.log(numbers);
+    } 
+     else if(displayValue == "/"){ 
+      number = arr.join('');
+      numbers.push(number);
+      numbers.push(displayValue);
+      
+      arr = [];
+      number = "";
+    } 
+    else{
+      arr.push(displayValue);
+      console.log(numbers);
+    
+    }
+    
    }
   });
 }
